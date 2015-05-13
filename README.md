38bc741a884270e001c36c448c79eef520b2567f
Gabriel Almeida <gabriel.almeida.rj@gmail.com>
2015-05-13 00:46:06 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+``` meteor add bigu:waypoints ```
---
+++ package.js
@@ -0,0 +1,14 @@
+Package.describe({
+  summary: "Wrapper for waypoints.js",
+  version: "0.0.1",
+  git: "https://github.com/bigu21/meteor-waypoints.git",
+  name: "bigu:waypoints"
+});
+
+Package.onUse(function(api) {
+  if(api.versionsFrom)
+    api.versionsFrom("METEOR@1.0");
+
+  api.addFiles(['waypoints.min.js'], 'client');
+  api.export("Waypoint");
+});
